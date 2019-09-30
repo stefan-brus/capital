6322392b0c16e83436ecf57134ba0e35bad008e8
Stefan Brus <stefan1brus@gmail.com>
2019-09-30 23:45:02 +0200

    Refactoring

--- game.js
+++ game.js
@@ -1,56 +1,70 @@
 class Game {
 
+    // --- INIT LOGIC ---
+
     constructor() {
-        this.capital = 0;
-        this.hour = 0;
-        this.day = 0;
-        this.age = 18;
+        this.state = {};
+        this.state.capital = 0;
+        this.state.hour = 0;
+        this.state.day = 0;
+        this.state.age = 18;
 
         this.views = [];
-    }
-
-    mainLoop() {
-        // UPDATE
-        this.capital++;
-
-        if (this.hour < 23) {
-            this.hour++;
-        }
-        else {
-            this.hour = 0;
-            this.day++;
-        }
-
-        const isLeapYear = (this.age % 4) == 0;
-
-        if ((isLeapYear && this.day > 365) || (!isLeapYear && this.day > 364)) {
-            this.day = 0;
-            this.age++;
-        }
-
-        // RENDER
-        this.views.forEach(view => view.update());
-    }
-
-    run() {
-        this.initViews();
-
-        setInterval(() => this.mainLoop(), 1000);
     }
 
     initViews() {
         const parentElement = document.body;
 
-        this.buildNumericView("capital", "Capital ($)", parentElement, () => this.capital);
-        this.buildNumericView("hour", "Hour", parentElement, () => this.hour);
-        this.buildNumericView("day", "Day", parentElement, () => this.day);
-        this.buildNumericView("age", "Age", parentElement, () => this.age);
+        this.buildNumericView("capital", "Capital ($)", parentElement, () => this.state.capital);
+        this.buildNumericView("hour", "Hour", parentElement, () => this.state.hour);
+        this.buildNumericView("day", "Day", parentElement, () => this.state.day);
+        this.buildNumericView("age", "Age", parentElement, () => this.state.age);
     }
 
     buildNumericView(name, label, parentElement, updater) {
         const view = new NumericView(name, label, parentElement, updater);
         view.create();
         this.views.push(view);
+    }
+
+    // --- GAME LOGIC ---
+
+    mainLoop() {
+        // UPDATE
+        this.updateCapital();
+        this.updateTime();
+
+        // RENDER
+        this.views.forEach(view => view.update());
+    }
+
+    updateCapital() {
+        this.state.capital++;
+    }
+
+    updateTime() {
+        if (this.state.hour < 23) {
+            this.state.hour++;
+        }
+        else {
+            this.state.hour = 0;
+            this.state.day++;
+        }
+
+        const isLeapYear = (this.state.age % 4) == 0;
+
+        if ((isLeapYear && this.state.day > 365) || (!isLeapYear && this.state.day > 364)) {
+            this.state.day = 0;
+            this.state.age++;
+        }
+    }
+
+    // --- ENTRY POINT ---
+
+    run() {
+        this.initViews();
+
+        setInterval(() => this.mainLoop(), 1000);
     }
 }
 
