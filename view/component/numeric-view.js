4212bced26a8a65e276c4fbb1cc65e73ced07dec
Stefan Brus <stefan1brus@gmail.com>
2019-10-16 00:27:19 +0200

    Make numeric view decimal number configurable

--- view/component/numeric-view.js
+++ view/component/numeric-view.js
@@ -1,11 +1,12 @@
 class NumericView {
 
-    constructor(name, label, parentElement, updater, isDecimal) {
+    constructor(name, label, parentElement, updater, isDecimal = false, numDecimals = 2) {
         this.name = name;
         this.label = label;
         this.parentElement = parentElement;
         this.updater = updater;
         this.isDecimal = isDecimal;
+        this.numDecimals = numDecimals;
     }
 
     create() {
@@ -15,13 +16,13 @@ class NumericView {
         this.viewElement = document.createElement("span");
         this.viewElement.id = `numeric-view-${this.name}`;
         const initVal = 0;
-        this.viewElement.textContent = this.isDecimal ? initVal.toFixed(2) : initVal;
+        this.viewElement.textContent = this.isDecimal ? initVal.toFixed(this.numDecimals) : initVal;
         element.appendChild(this.viewElement);
         this.parentElement.appendChild(element);
     }
 
     update() {
         const newVal = this.updater();
-        this.viewElement.textContent = this.isDecimal ? newVal.toFixed(2) : newVal;
+        this.viewElement.textContent = this.isDecimal ? newVal.toFixed(this.numDecimals) : newVal;
     }
 }
